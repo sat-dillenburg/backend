f6a60189062c442dbf7a1abd6e4030b617509b78
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-10-10 00:58:16 +0200

    Add github workflow trigger hook

---
+++ directus/public/extensions/custom/hooks/build-frontend.php
@@ -0,0 +1,43 @@
+<?php
+
+$GITHUB_TOKEN = '';
+
+function triggerGithubWorkflow() {
+  $client = new \GuzzleHttp\Client();
+
+  $data = [
+    'event_type' => 'build-interim',
+  ];
+
+  $client->request(
+    'POST',
+    'https://api.github.com/repos/sat-dillenburg/frontend/dispatches',
+    [
+      'json' => $data,
+      'headers' => [
+        'User-Agent' => 'directus',
+        'Authorization' => 'token ' . $GITHUB_TOKEN,
+      ],
+    ]
+  );
+}
+
+return [
+  'actions' => [
+    'item.create.pages' => function () {
+      return triggerGithubWorkflow();
+    },
+    'item.create.events' => function () {
+      return triggerGithubWorkflow();
+    },
+    'item.update.sat_interim' => function () {
+      return triggerGithubWorkflow();
+    },
+    'item.update.pages' => function () {
+      return triggerGithubWorkflow();
+    },
+    'item.update.events' => function () {
+      return triggerGithubWorkflow();
+    },
+  ]
+];
